f6f6d2099e831d40613f927b96cf9a5d8629b345
Steven Clontz <steven.clontz@gmail.com>
2020-09-05 22:57:43 +0000

    start to implement Bootstrap

--- src/App.svelte
+++ src/App.svelte
@@ -1,43 +1,67 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+
+    import {
+      Collapse,
+      Navbar,
+      NavbarToggler,
+      NavbarBrand,
+      Nav,
+      NavItem,
+      NavLink,
+      UncontrolledDropdown,
+      DropdownToggle,
+      DropdownMenu,
+      DropdownItem,
+      Container
+    } from 'sveltestrap';
+  
+    let navIsOpen = false;
+  
+    function handleUpdate(event) {
+      navIsOpen = event.detail.isOpen;
+	}
+
 	let bank = {
 		"title": "Loading...",
 		"slug": "loading",
 		"outcomes": [],
-	}
+	};
 	onMount(async () => {
 		const res = await fetch(`bank.json`);
 		bank = await res.json();
 	});
 </script>
 
-<main>
-	<h1>{bank.title}</h1>
-</main>
+<Navbar color="primary" dark expand="md">
+  <NavbarBrand href="/">☑️It</NavbarBrand>
+  <NavbarToggler on:click={() => (navIsOpen = !navIsOpen)} />
+  <Collapse {navIsOpen} navbar expand="md" on:update={handleUpdate}>
+    <Nav navbar>
+      <NavItem>
+        <NavLink href="#">Bank 1</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">Bank 2</NavLink>
+      </NavItem>
+      <UncontrolledDropdown nav inNavbar>
+        <DropdownToggle nav caret>Options</DropdownToggle>
+        <DropdownMenu right>
+          <DropdownItem>Option 1</DropdownItem>
+          <DropdownItem>Option 2</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Reset</DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    </Nav>
+  </Collapse>
+</Navbar>
+
+<Container>
+<h1>{bank.title}</h1>
 {#each bank.outcomes as outcome}
 <pre>{outcome.name}</pre>
 {:else}
 <pre>Loading...</pre>
 {/each}
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+</Container>
