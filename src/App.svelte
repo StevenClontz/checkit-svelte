d6cdb15f8fb790e5b7d0db440601462fb0b6acd6
Steven Clontz <steven.clontz@gmail.com>
2020-09-05 21:09:04 +0000

    import bank

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
-	export let name: string;
+	import { onMount } from 'svelte';
+	let bank = {
+		"title": "Loading...",
+		"slug": "loading",
+		"outcomes": [],
+	}
+	onMount(async () => {
+		const res = await fetch(`bank.json`);
+		bank = await res.json();
+	});
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>{bank.title}</h1>
 </main>
+{#each bank.outcomes as outcome}
+<pre>{outcome.name}</pre>
+{:else}
+<pre>Loading...</pre>
+{/each}
 
 <style>
 	main {
