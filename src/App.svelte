8200a5ff9f3f6845a3db126e359e285975f63213
Steven Clontz <steven.clontz@gmail.com>
2020-09-06 15:47:52 +0000

    add jumbotron

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,8 @@
       DropdownToggle,
       DropdownMenu,
       DropdownItem,
-      Container
+      Container,
+      Jumbotron,
     } from 'sveltestrap';
   
     let navIsOpen = false;
@@ -57,6 +58,36 @@
   </Collapse>
 </Navbar>
 
+<Jumbotron>
+  <Container>
+    <h1 class="display-3">☑️It</h1>
+    <p class="lead">Free and open randomized exercises for practice and assessment.</p>
+
+    <ul>
+      <li>
+        <span class="lead">For <strong>students</strong>:</span> Use these exercises to ✔️
+        your understanding of your course's learning outcomes.
+      </li>
+      <li>
+        <span class="lead">For <strong>instructors</strong>:</span> Use these exercises in
+        assessments to help you ✔️ on student understanding.
+      </li>
+    </ul>
+
+    <p class="lead">
+      <UncontrolledDropdown color="primary">
+        <DropdownToggle caret>Select an exercise bank:</DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem header>Header</DropdownItem>
+          <DropdownItem disabled>Action</DropdownItem>
+          <DropdownItem>Another Action</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Another Action</DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    </p>
+  </Container>
+</Jumbotron>
 <Container>
 <h1>{bank.title}</h1>
 {#each bank.outcomes as outcome}
