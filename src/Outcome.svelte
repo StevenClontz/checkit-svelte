bedf94acaaf225b8e0692ba83a2672579d96b2de
Steven Clontz <steven.clontz@gmail.com>
2020-09-08 02:43:06 +0000

    try out katex

--- src/Outcome.svelte
+++ src/Outcome.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
     import Pagination from './Pagination.svelte';
-    import type { Outcome } from './types';
-    import { parseMath } from './mathjax.js';
+    import type { Outcome, Exercise } from './types';
+    //import katex from 'katex';
 
     export let outcome: Outcome;
 
     let version = 0;
+
+    let inlineMathRe = /\\\((.*)\\\)/;
+    let displayMathRe = /\\\[(.*)\\\]/;
+    const parseMath = (html:string) => {
+        //html = html.replace(inlineMathRe, katex.renderToString);
+        //return html.replace(displayMathRe, katex.renderToString);
+        return html
+    };
+    const preview = (e:Exercise) => {
+        return parseMath(e.html)
+    }
 </script>
 
 <h3>{outcome.slug} - {outcome.title}</h3>
@@ -15,5 +26,4 @@
     <Pagination bind:page={version} pages={outcome.exercises.length}/>
 </div>
 
-<!--{@html parseMath(outcome.exercises[version].html)}-->
-{@html outcome.exercises[version].html}
+{@html preview(outcome.exercises[version])}
