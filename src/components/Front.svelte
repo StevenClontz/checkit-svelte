16a8696c30a4441e03ed79127e8ce38e0af71551
Steven Clontz <steven.clontz@gmail.com>
2021-01-23 22:00:04 +0000

    add demos to front page

--- src/components/Front.svelte
+++ src/components/Front.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { Container } from 'sveltestrap';
+    import { Container, Row, Col } from 'sveltestrap';
 </script>
 
 <main>
@@ -12,10 +12,19 @@
 		<p>On <strong>iOS/Safari</strong>: tap the share arrow,
 		then tap "Add to Home Screen".</p>
 
-		<h4>Looking to author your own randomized exercises?</h4>
-		
-		<p>Check out our <a href="https://github.com/stevenclontz/checkit">README</a> on Github.</p>
-		
+		<h4>Looking to author your own exercises?</h4>
+
+		<p>Check out our <a href="https://github.com/stevenclontz/checkit">README</a> to get started!</p>
+
+		<Row>
+			<Col>
+				<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Using <a href="https://twitter.com/CheckItProblems?ref_src=twsrc%5Etfw">@CheckItProblems</a> you can create randomized Diff EQ or Linear Algebra quiz PDFs with answer keys in 83 seconds. No signup required. Watch here to see for yourself, or go to <a href="https://t.co/PCPhuo5Eha">https://t.co/PCPhuo5Eha</a> and give it a try. <a href="https://t.co/ypFBUutYvY">pic.twitter.com/ypFBUutYvY</a></p>&mdash; CheckIt Problems (@CheckItProblems) <a href="https://twitter.com/CheckItProblems/status/1337126028481884175?ref_src=twsrc%5Etfw">December 10, 2020</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+			</Col>
+			<Col>
+				<iframe width="560" height="315" title="Instructor Demo YouTube" src="https://www.youtube.com/embed/84lKf2eadro" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</Col>
+		</Row>
+
 		<h4>Source on GitHub</h4>
 		
 		<ul>
